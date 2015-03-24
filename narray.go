d902b93a45eff57a5515d583d5c095325ee05ceb
Leo Neumeyer <leoneu@apache.org>
2015-03-24 16:38:12 -0700

    Clean up.

--- narray.go
+++ narray.go
@@ -280,7 +280,6 @@ func AddScaled(y *NArray, x *NArray, a float64) *NArray {
 			panic("narrays must have equal shape.")
 		}
 	}
-	//	for i := 0; i < len(y.Data); i++ {
 	for i, v := range x.Data {
 		y.Data[i] += v * a
 	}
